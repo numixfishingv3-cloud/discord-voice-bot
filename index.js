f578a1e7ae9d49109962e59897af876c44290b3c
numixfishingv3-cloud <numixfishingv3@gmail.com>
2026-03-16 14:03:32 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const play = require("play-dl");
+const { createAudioPlayer, createAudioResource } = require("@discordjs/voice");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ],
 });
 
-client.once('clientReady', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+const player = createAudioPlayer();
 
-  const channel = client.channels.cache.get('1248950710331113542');
+client.once("ready", () => {
+
+  console.log(`ล็อกอินแล้ว ${client.user.tag}`);
+
+  const channel = client.channels.cache.get("1248950710331113542"); // ใส่ Voice ID
 
   if (!channel) {
     console.log("หา Voice Channel ไม่เจอ");
     return;
   }
 
-  joinVoiceChannel({
+  const connection = joinVoiceChannel({
     channelId: channel.id,
     guildId: channel.guild.id,
     adapterCreator: channel.guild.voiceAdapterCreator,
   });
 
-  console.log("Bot joined voice channel");
+  connection.subscribe(player);
+
+  console.log("บอทเข้า Voice แล้ว (24/7)");
+
+});
+
+client.on("interactionCreate", async interaction => {
+
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    interaction.reply("บอททำงานอยู่ 🟢");
+  }
+
+  if (interaction.commandName === "play") {
+
+    const url = interaction.options.getString("url");
+
+    const stream = await play.stream(url);
+
+    const resource = createAudioResource(stream.stream);
+
+    player.play(resource);
+
+    interaction.reply("กำลังเปิดเพลง 🎵");
+
+  }
+
 });
 
 client.login(process.env.TOKEN);
